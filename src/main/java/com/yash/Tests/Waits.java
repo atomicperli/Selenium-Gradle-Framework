9b5f26baf301a72c7892ecc8371bd28974d01e58
yperli <yperli@cisco.com>
2021-12-31 15:28:21 +0530

    Adding folder to gitignore

--- src/main/java/com/yash/Tests/Waits.java
+++ src/main/java/com/yash/Tests/Waits.java
@@ -54,7 +54,7 @@ public class Waits extends InitialSuiteSetup {
 
         //Assert Title of The Page
         System.out.println(driver.getTitle());
-        Assert.assertEquals(driver.getTitle(), "ToolsQAA");
+        Assert.assertEquals(driver.getTitle(), "ToolsQA");
 
         //Assert fullName field text and Enter value
         String fullNameField = uiHelper.getText(driver, (String) JsonTestData.get("fullNameTextBoxField"), 60);
